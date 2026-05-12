8df40392f2f8ce27cd07dcae06a1bbddd1328c3a
miunibm <175016327+miunibm@users.noreply.github.com>
2026-05-12 21:38:07 +0000

    Try out buildah instead of docker
    Signed-off-by: miunibm <175016327+miunibm@users.noreply.github.com>

---
+++ pipelines/azure/scripts/install-buildah-ubuntu.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+echo "Updating existing OS packages..."
+sudo apt-get -qy update
+
+echo "Upgrading existing OS packages..."
+sudo apt-get -qy upgrade
+
+echo "Installing buildah prerequisites ..."
+sudo apt-get -qy install \
+	ca-certificates \
+	curl \
+	fuse-overlayfs
+	gnupg2
+
+. /etc/os-release
+echo "Installing buildah for OS release ${VERSION_ID}..."
+echo "deb http://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/xUbuntu_${VERSION_ID}/ /" | sudo tee /etc/apt/sources.list.d/devel:kubic:libcontainers:stable.list
+curl -fsL "https://download.opensuse.org/repositories/devel:kubic:libcontainers:stable/xUbuntu_${VERSION_ID}/Release.key" | sudo apt-key add - &&
+sudo apt-get -qq -y update
+
+sudo apt-get -qq -y install buildah
+
+if [ ! "$(buildah version)" ] ; then
+  echo "FATAL: Buildah is not available! Cannot continue"
+  exit 3
+fi
